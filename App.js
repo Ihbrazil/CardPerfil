cf7ab1314aa7bc438035059475720f8664e40d52
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-16 19:48:01 -0300

    Atualização da 2ª Missão da Carreira Programador Mobile
    2ª Missão da Carreira Programador Mobile - O objetivo desse projeto é criar o primeiro aplicativo mobile utilizando o React Native. Colocar em prática os seguintes conceitos: Uso do View (agrupar componentes),  do Text (exibir textos), do Image (Exibir imagens) e  do StyleSheet (criar estilos).

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Capa from "./assets/images/capa.png";
+import Foto from "./assets/images/foto.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Image style={estilos.capa} source={ Capa } />
+      <View style={estilos.boxDesenvolvedor}>
+          <Image style={estilos.foto} source={ Foto } />
+          <Text style={estilos.titulo}>Isaias Horta Brazil</Text>
+          <Text style={estilos.texto}>Bacharel em Sistemas de Informação e desenvolvedor FullStack, especializado nas tecnologias JavaScript, React e Reat Native</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  capa: {
+    flex: 1,
+  },
+
+  boxDesenvolvedor: {
+    flex: 1,
+    borderColor: "#fff",
+    alignItems: "center",
+  },
+
+  foto: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+
+  titulo: {
+    fontSize: 22,
+    marginBottom: 10,
+    fontWeight: "normal",
+  },
+
+  texto: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: "normal",
+    marginHorizontal: 10,
   },
 });
